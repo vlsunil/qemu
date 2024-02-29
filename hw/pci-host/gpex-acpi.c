63f7b78c0edb76bc09afe9a5f2a7410f9678e69c
Sunil V L <sunilvl@ventanamicro.com>
2024-02-29 21:22:18 +0530

    hw/pci-host/gpex-acpi.c: Add support to add _DEP
    Some architectures like RISC-V need to provide the dependency on
    interrupt controller for namespace devices if they use IRQ resource. PCI
    host bridge also need it for INTx routing. So, add support to create the
    _DEP object if the caller initializes it.
    
    Signed-off-by: Sunil V L <sunilvl@ventanamicro.com>

--- hw/pci-host/gpex-acpi.c
+++ hw/pci-host/gpex-acpi.c
@@ -7,7 +7,7 @@
 #include "hw/pci/pcie_host.h"
 #include "hw/acpi/cxl.h"
 
-static void acpi_dsdt_add_pci_route_table(Aml *dev, uint32_t irq)
+static void acpi_dsdt_add_pci_route_table(Aml *scope, Aml *dev, uint32_t irq)
 {
     Aml *method, *crs;
     int i, slot_no;
@@ -45,7 +45,8 @@ static void acpi_dsdt_add_pci_route_table(Aml *dev, uint32_t irq)
         aml_append(dev_gsi, aml_name_decl("_CRS", crs));
         method = aml_method("_SRS", 1, AML_NOTSERIALIZED);
         aml_append(dev_gsi, method);
-        aml_append(dev, dev_gsi);
+
+        aml_append(scope, dev_gsi);
     }
 }
 
@@ -174,7 +175,12 @@ void acpi_dsdt_add_gpex(Aml *scope, struct GPEXConfig *cfg)
                 aml_append(dev, aml_name_decl("_PXM", aml_int(numa_node)));
             }
 
-            acpi_dsdt_add_pci_route_table(dev, cfg->irq);
+            acpi_dsdt_add_pci_route_table(scope, dev, cfg->irq);
+            if (cfg->irq_dep != NULL) {
+                Aml *dep_pkg = aml_package(1);
+                aml_append(dep_pkg, aml_name("%s", cfg->irq_dep));
+                aml_append(dev, aml_name_decl("_DEP", dep_pkg));
+            }
 
             /*
              * Resources defined for PXBs are composed of the following parts:
@@ -205,7 +211,12 @@ void acpi_dsdt_add_gpex(Aml *scope, struct GPEXConfig *cfg)
     aml_append(dev, aml_name_decl("_STR", aml_unicode("PCIe 0 Device")));
     aml_append(dev, aml_name_decl("_CCA", aml_int(1)));
 
-    acpi_dsdt_add_pci_route_table(dev, cfg->irq);
+    acpi_dsdt_add_pci_route_table(scope, dev, cfg->irq);
+    if (cfg->irq_dep != NULL) {
+        Aml *dep_pkg = aml_package(1);
+        aml_append(dep_pkg, aml_name("%s", cfg->irq_dep));
+        aml_append(dev, aml_name_decl("_DEP", dep_pkg));
+    }
 
     method = aml_method("_CBA", 0, AML_NOTSERIALIZED);
     aml_append(method, aml_return(aml_int(cfg->ecam.base)));
@@ -282,13 +293,17 @@ void acpi_dsdt_add_gpex(Aml *scope, struct GPEXConfig *cfg)
     crs_range_set_free(&crs_range_set);
 }
 
-void acpi_dsdt_add_gpex_host(Aml *scope, uint32_t irq)
+void acpi_dsdt_add_gpex_host(Aml *scope, uint32_t irq, const char *irq_dep)
 {
     bool ambig;
     Object *obj = object_resolve_path_type("", TYPE_GPEX_HOST, &ambig);
 
     if (!obj || ambig) {
         return;
+    }
+
+    if (irq_dep) {
+        GPEX_HOST(obj)->gpex_cfg.irq_dep = irq_dep;
     }
 
     GPEX_HOST(obj)->gpex_cfg.irq = irq;
