e1bd8e070aa70ea1e10af6ea3a4ca4360065e165
Alexandre Ghiti <alexghiti@rivosinc.com>
2024-07-18 18:07:37 +0200

    target: riscv: Add Svvptc extension support
    The Svvptc extension describes a uarch that does not cache invalid TLB
    entries: that's the case for qemu so there is nothing particular to
    implement other than the introduction of this extension.
    
    Since qemu already exposes Svvptc behaviour, let's enable it by default
    since it allows to drastically reduce the number of sfence.vma emitted
    by S-mode.
    
    Signed-off-by: Alexandre Ghiti <alexghiti@rivosinc.com>

--- target/riscv/cpu.c
+++ target/riscv/cpu.c
@@ -197,6 +197,7 @@ const RISCVIsaExtData isa_edata_arr[] = {
     ISA_EXT_DATA_ENTRY(svinval, PRIV_VERSION_1_12_0, ext_svinval),
     ISA_EXT_DATA_ENTRY(svnapot, PRIV_VERSION_1_12_0, ext_svnapot),
     ISA_EXT_DATA_ENTRY(svpbmt, PRIV_VERSION_1_12_0, ext_svpbmt),
+    ISA_EXT_DATA_ENTRY(svvptc, PRIV_VERSION_1_12_0, ext_svvptc),
     ISA_EXT_DATA_ENTRY(ssnpm, PRIV_VERSION_1_12_0, ext_ssnpm),
     ISA_EXT_DATA_ENTRY(smnpm, PRIV_VERSION_1_12_0, ext_smnpm),
     ISA_EXT_DATA_ENTRY(smmpm, PRIV_VERSION_1_12_0, ext_smmpm),
@@ -1501,6 +1502,7 @@ const RISCVCPUMultiExtConfig riscv_cpu_extensions[] = {
     MULTI_EXT_CFG_BOOL("svinval", ext_svinval, false),
     MULTI_EXT_CFG_BOOL("svnapot", ext_svnapot, false),
     MULTI_EXT_CFG_BOOL("svpbmt", ext_svpbmt, false),
+    MULTI_EXT_CFG_BOOL("svvptc", ext_svvptc, true),
 
     MULTI_EXT_CFG_BOOL("zicntr", ext_zicntr, true),
     MULTI_EXT_CFG_BOOL("zihpm", ext_zihpm, true),
